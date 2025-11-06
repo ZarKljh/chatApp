701813b458b6ba87e5aa42cf7c406f51c2ff8278
ZarKljh <zarkljh@gmail.com>
2025-11-06 15:43:32 +0900

    init data

--- src/main/java/com/ll/chatApp/domain/chat/chatMessage/controller/ApiChatMessageController.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatMessage/controller/ApiChatMessageController.java
@@ -1,7 +1,21 @@
 package com.ll.chatApp.domain.chat.chatMessage.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiChatMessageController {
+    @GetMapping("/api/v1/chat/rooms/{roomId}/message")
+    public String getChatMessages(
+            @PathVariable("roomId") Long roomId,
+            @RequestParam(value="afterChatMessageId", defaultValue = "-1") Long afterChatMessageId
+    ){
+        return roomId + "번 채팅방 메세지 목록 조회 완료 : id : " + afterChatMessageId;
+    }
+
+    @PostMapping("/api/v1/chat/rooms/{roomId}/message")
+    public String createChatMessages(
+            @PathVariable("roomId") Long roomId
+    ){
+        return roomId + "번 채팅방 메세지 생성완료";
+    }
 }
--- src/main/java/com/ll/chatApp/domain/chat/chatRoom/controller/ApiChatRoomController.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/controller/ApiChatRoomController.java
@@ -1,7 +1,24 @@
 package com.ll.chatApp.domain.chat.chatRoom.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/chat/rooms")
 public class ApiChatRoomController {
+
+    @GetMapping
+    public String getChatRooms(){
+        return "채팅방 목록 조회 완료";
+    }
+
+    @GetMapping("/{roomId}")
+    public String getChatRoom(@PathVariable("roomId") Long roomId){
+        return roomId + "번 채팅방 목록 조회 완료";
+    }
+
+    @PostMapping
+    public String createChatRoom(){
+        return "채팅방 생성 완료";
+    }
+
 }
