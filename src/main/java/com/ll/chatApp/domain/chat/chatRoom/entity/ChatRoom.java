fd003a1d81399fc44f6ad9203395a0764782504a
ZarKljh <zarkljh@gmail.com>
2025-11-06 14:46:13 +0900

    baseEntity add

---
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/entity/ChatRoom.java
@@ -0,0 +1,16 @@
+package com.ll.chatApp.domain.chat.chatRoom.entity;
+
+import com.ll.chatApp.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@ToString(callSuper = true)
+public class ChatRoom extends BaseEntity {
+}
---
+++ src/main/java/com/ll/chatApp/global/rsData/RsData.java
@@ -0,0 +1,4 @@
+package com.ll.chatApp.global.rsData;
+
+public class RsData {
+}
