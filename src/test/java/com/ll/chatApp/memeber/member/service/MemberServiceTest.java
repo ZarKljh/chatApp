e9b26524925e7c427b2fd2a4c774f11219a4c573
ZarKljh <zarkljh@gmail.com>
2025-11-07 14:51:48 +0900

    join test

---
+++ src/test/java/com/ll/chatApp/memeber/member/service/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package com.ll.chatApp.memeber.member.service;
+
+import com.ll.chatApp.domain.member.member.entity.Member;
+import com.ll.chatApp.domain.member.member.service.MemberService;
+import com.ll.chatApp.global.rsData.RsData;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test") // 테스트를 할 때에도 application.yml을 사용하겠다는 의미이다.
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    private MemberService memberService;
+
+    @DisplayName("회원가입")
+    @Test
+    void t1(){
+        RsData<Member> joinRs = memberService.join("usernew", "1234");
+        Member member = joinRs.getData();
+        assertThat(member.getId()).isGreaterThan(0L);
+    }
+}
